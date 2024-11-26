603556f229782ff00b941eb7e4811cec0594e04c
Caroline Fairhurst <cfairhurst@protonmail.com>
2024-11-26 14:51:57 -0500

    wip cart

--- src/routes/cart/get_cart.js
+++ src/routes/cart/get_cart.js
@@ -1,6 +1,20 @@
-export const handler = async (event) => {
-    console.log(event)
+import { middyfy } from "#lib/middleware.js";
+
+const getCartHandler = async (event) => {
+  console.log("my  event is ", event);
+  try {
+    //const cart = await getCart(uuid);
+    //return {
+    // statusCode: 200,
+    //  body: JSON.stringify(cart),
+    //  };
+  } catch (error) {
+    console.error(error);
     return {
-        statusCode: 200
-    }
-}
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
+};
+
+export const handler = middyfy(getCartHandler);
