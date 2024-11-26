1760ddc2c536522ee4b71bea9b4f2d4d35b7c7b6
Caroline Fairhurst <cfairhurst@protonmail.com>
2024-11-26 15:26:45 -0500

    cart schema

--- src/routes/cart/get_cart.js
+++ src/routes/cart/get_cart.js
@@ -1,13 +1,35 @@
 import { middyfy } from "#lib/middleware.js";
+import { path } from "ramda";
+import { getCart } from "#lib/services/dynamodb/index.js";
 
 const getCartHandler = async (event) => {
   console.log("my  event is ", event);
   try {
-    //const cart = await getCart(uuid);
-    //return {
-    // statusCode: 200,
-    //  body: JSON.stringify(cart),
-    //  };
+    const uuid = path(
+      ["requestContext", "authorizer", "jwt", "claims", "username"],
+      event
+    );
+
+    if (!uuid) {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({ message: "Unauthorized" }),
+      };
+    }
+
+    const cart = await getCart(uuid);
+
+    if (!cart) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "Cart not found" }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(cart),
+    };
   } catch (error) {
     console.error(error);
     return {
