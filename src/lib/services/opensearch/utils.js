9bb130b240cd40d398b262fcc4689e287efb7ae7
Caroline Fairhurst <cfairhurst@protonmail.com>
2024-12-31 12:38:15 -0500

    serverless --> managed oss
    managed deploys
    
    query works
    
    nodes

--- src/lib/services/opensearch/utils.js
+++ src/lib/services/opensearch/utils.js
@@ -16,17 +16,16 @@ import { INDEX_NAME, PRODUCTS_MAPPINGS } from "./config.js";
 
 export const makeClient = () =>
   new Client({
-    node: "https://lz0pc9525ehv55a8p6se.us-east-1.aoss.amazonaws.com",
     ...AwsSigv4Signer({
       /*region: process.env.AWS_REGION,*/
       region: "us-east-1",
-      service: "aoss",
+      service: "es",
       getCredentials: () => {
         const credentialsProvider = defaultProvider();
         return credentialsProvider();
       },
     }),
-    // node: process.env.PRODUCTS_COLLECTION_ENDPOINT, // OpenSearch domain URL
+    node: process.env.PRODUCTS_DOMAIN_ENDPOINT, // OpenSearch domain URL
   });
 
 const addIndexName = assoc("index", INDEX_NAME);
@@ -66,6 +65,8 @@ export const formatQuery = pipe(
   baseQuery,
   addIndexName
 );
+
+export const formatTestQuery = pipe(baseQuery, addIndexName);
 
 export const formatCategoryQuery = pipe(
   split(","),
